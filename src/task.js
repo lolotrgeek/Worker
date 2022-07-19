040b8fd4ec324869d9b0c15e785818cae1563723
lolotrgeek <jon@simplyenable.com>
2022-07-19 08:40:44 -0500

    feat: task id & creator

--- src/task.js
+++ src/task.js
@@ -1,37 +1,19 @@
 const { randomUUID } = require('crypto')
 
 class Task {
-    constructor() {
-        this.id = randomUUID()
+    constructor(creator) {
+        this.id = "task_"+randomUUID()
+        this.creator = creator ? creator : ""
         this.reward = 0
         this.status = '' // done, doing, completed
 
         // task types: labor , validate
     }
 
-    complete() {
-        // a set of tests to verify that this is complete
+    conditions() {
+        return true
     }
 
-    distribute() {
-        // the rules for distributing the impact
-
-        // 80% distributed to completer, 20% to verifiers
-    }
-
-    generate() {
-        // the more other tasks are linked to the product of this task, the more impact this generates
-
-        // downstream value generation trickles up
-    }
-    update() {
-
-    }
-    
-    start() {
-
-    }
-    
 }
 
 module.exports = { Task }
